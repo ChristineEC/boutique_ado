38791ee009d8db9ddcc8cb16c83aca0ea6a67745
ChristineEC <carterchristinee@gmail.com>
2025-01-30 14:38:52 +0100

    Fix cut and paste error in base html

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_48ybgwe2f&3%wargi)d)dz3^lx*fdgbs$@&@z23!(u+mbx!ue'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-christineec-boutiqueado-svnh5h8m0ui.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
