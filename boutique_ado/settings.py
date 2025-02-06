f68a18e3ea42609772180e62f5e71652819b8576
ChristineEC <carterchristinee@gmail.com>
2025-02-06 15:51:54 +0100

    Retry setting allowed host

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["boutiqueadocec.herokupp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["boutiqueadocec-86ad819bf9fa.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
