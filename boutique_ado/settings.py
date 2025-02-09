69c1aba86e523d0e2f64554360ad6f9ce07c5a41
ChristineEC <carterchristinee@gmail.com>
2025-02-09 14:46:36 +0100

    Set csrf trusted origins

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,13 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+
 
 
 ALLOWED_HOSTS = ["127.0.0.1", "boutiqueadocec-86ad819bf9fa.herokuapp.com"]
 
-# Application definition
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000/',
+    'https://boutiqueadocec-86ad819bf9fa.herokuapp.com/',
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
