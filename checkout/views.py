fc1565a31a9b0221a424b038f171296e3cd179fb
ChristineEC <carterchristinee@gmail.com>
2025-02-09 13:55:07 +0100

    Fix autoformat errors in checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,4 @@
-from django.shortcuts import (
-    render,
-    redirect,
-    reverse,
-    get_object_or_404,
-    HttpResponse,
-)
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
@@ -31,15 +25,14 @@ def cache_checkout_data(request):
                 "bag": json.dumps(request.session.get("bag", {})),
                 "save_info": request.POST.get("save_info"),
                 "username": request.user,
-            },
+            }
         )
         return HttpResponse(status=200)
     except Exception as e:
         messages.error(
             request,
             "Sorry, your payment cannot be \
-                       processed right now. Please try again later.",
-        )
+                processed right now. Please try again later.")
         return HttpResponse(content=e, status=400)
 
 
@@ -82,9 +75,7 @@ def checkout(request):
                         order_line_item.save()
                     else:
                         # with sizes
-                        for size, quantity in item_data[
-                            "items_by_size"
-                        ].items():
+                        for size, quantity in item_data["items_by_size"].items():
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
@@ -93,32 +84,22 @@ def checkout(request):
                             )
                             order_line_item.save()
                 except Product.DoesNotExist:
-                    messages.error(
-                        request,
-                        (
-                            "One of the products in your bag wasn't found \
-                        in our database. "
-                            "Please call us for assistance!"
-                        ),
+                    messages.error(request, (
+                            "One of the products in your bag wasn't found"
+                            "in our database. Please call us for assistance!")
                     )
                     order.delete()
                     return redirect(reverse("view_bag"))
+            # Save the info to the user's profile if all is well
             request.session["save_info"] = "save-info" in request.POST
-            return redirect(
-                reverse("checkout_success", args=[order.order_number])
-            )
+            return redirect(reverse("checkout_success", args=[order.order_number]))
         else:
-            messages.error(
-                request,
-                "There was an error with your form. \
-                           Please double check your information.",
-            )
+            messages.error(request, "There was an error with your form. \
+                           Please double check your information.")
     else:
         bag = request.session.get("bag", {})
         if not bag:
-            messages.error(
-                request, "There is nothing in your bag at the moment"
-            )
+            messages.error(request, "There is nothing in your bag at the moment")
             return redirect(reverse("products"))
 
         current_bag = bag_contents(request)
@@ -129,12 +110,11 @@ def checkout(request):
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
         )
-
+        # Attempt to prefill the form with any info the user maintains in their profile
         if request.user.is_authenticated:
             try:
                 profile = UserProfile.objects.get(user=request.user)
-                order_form = OrderForm(
-                    initial={
+                order_form = OrderForm(initial={
                         "full_name": profile.user.get_full_name(),
                         "email": profile.user.email,
                         "phone_number": profile.default_phone_number,
@@ -144,19 +124,16 @@ def checkout(request):
                         "street_address1": profile.default_street_address1,
                         "street_address2": profile.default_street_address2,
                         "county": profile.default_county,
-                    }
-                )
+                    })
             except UserProfile.DoesNotExist:
                 order_form = OrderForm()
         else:
             order_form = OrderForm()
 
     if not stripe_public_key:
-        messages.warning(
-            request,
+        messages.warning(request,
             "Stripe public key is missing. \
-                         Did you forget to set it in your environment?",
-        )
+                Did you forget to set it in your environment?")
 
     template = "checkout/checkout.html"
     context = {
@@ -196,12 +173,9 @@ def checkout_success(request, order_number):
             if user_profile_form.is_valid():
                 user_profile_form.save()
 
-    messages.success(
-        request,
-        f"Order successfully processed! \
-                     Your order number is {order_number}. A confirmation \
-                        email will be sent to {order.email}.",
-    )
+    messages.success(request, f"Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.")
 
     if "bag" in request.session:
         del request.session["bag"]
