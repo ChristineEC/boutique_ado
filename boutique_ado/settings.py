0a434f8e8609777c06f06994dc75aeba77088607
ChristineEC <carterchristinee@gmail.com>
2025-02-12 15:08:09 +0100

    Add custom 404 page

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,13 +26,17 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-
-
-ALLOWED_HOSTS = ["127.0.0.1", "boutiqueadocec-86ad819bf9fa.herokuapp.com"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "boutiqueadocec-86ad819bf9fa.herokuapp.com",
+    "localhost",
+    "https://865rjbfs-8000.euw.devtunnels.ms/"
+    ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000/",
     "https://boutiqueadocec-86ad819bf9fa.herokuapp.com/",
+    "https://865rjbfs-8000.euw.devtunnels.ms/"
 ]
 
 INSTALLED_APPS = [
